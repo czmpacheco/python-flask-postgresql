55e17928559f4d2c6d155096e979eb3fec79b4aa
Carla Pacheco <51806625+czmpacheco@users.noreply.github.com>
2019-06-26 20:45:27 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -24,8 +24,8 @@ def create_app(env_name):
     bcrypt.init_app(app)
     db.init_app(app)
 
-    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
-    app.register_blueprint(blogpost_blueprint, url_prefix='/api/v1/blogposts')
+    app.register_blueprint(user_blueprint, url_prefix='/users')
+    app.register_blueprint(blogpost_blueprint, url_prefix='/blogposts')
 
     @app.route('/', methods=['GET'])
     def index():
